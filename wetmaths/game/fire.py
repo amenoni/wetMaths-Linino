05731bd5e4c17946cdd437226932fc7e06b9ae3d
amenoni <amenoni@datafarming.net>
2016-03-22 09:22:58 -0300

    work in progress

--- wetmaths/game/fire.py
+++ wetmaths/game/fire.py
@@ -5,3 +5,6 @@ firebase = firebase.FirebaseApplication('https://resplendent-torch-6152.firebase
 def sendMessage(message):
     #use example, we are calling this method in game post_save
     result = firebase.post('/mensaje',message)
+
+def sendGameMessage(gameId,key,message):
+    firebase.post('/game_%s/%s' % (gameId,key),message)
--- wetmaths/game/models.py
+++ wetmaths/game/models.py
@@ -3,24 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core import serializers
 from hw.hwInterface import sweepMode
-from fire import sendMessage
+import fire
 
 
 # Create your models here.
 class Game(models.Model):
-    WINNER_CHOICES = (
-    ('1', 'First Player'),
-    ('2', 'Second Player'),
-    ('3', 'Third Player'),
-    )
-
-    STATUS_CHOICES = (
-        ('n','Not Started'),
-        ('s','Started'),
-        ('f','Finished'),
-        ('c','Cancelled'),
-    )
+    WINNER_CHOICES = ((1, 'First Player'),(2, 'Second Player'),(3, 'Third Player'),)
+    STATUS_CHOICES = (('n','Not Started'),('s','Started'),('f','Finished'),('c','Cancelled'),)
 
     started_at = models.DateTimeField()
     ended_at = models.DateTimeField(null=True,blank=True)
@@ -28,13 +19,12 @@ class Game(models.Model):
     player2 = models.TextField(null=True,blank=True)
     player3 = models.TextField(null=True,blank=True)
     winner = models.IntegerField(null=True,blank=True,choices=WINNER_CHOICES )
-    status = models.TextField(max_length=1,choices=STATUS_CHOICES)
+    status = models.TextField(choices=STATUS_CHOICES)
 
 
 #Game Post Save
 @receiver(post_save, sender=Game)
 def launch_game(sender, instance, **kwargs):
-    print "HOLA QUE HACE!!!!! %s" % instance.started_at
-    #TODO uncoment
-    sendMessage("jajajajajaja")
+    json = serializers.serialize('json',[instance,])
+    fire.sendGameMessage(instance.pk,"game_update",json)
     #sweepMode()
