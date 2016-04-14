51c8ebe7d862a75e6a2aa115e9558e98426840fa
amenoni <amenoni@datafarming.net>
2016-04-14 09:21:50 -0300

    moves api gets unicode values an inserts integer values

--- wetmaths/game/api.py
+++ wetmaths/game/api.py
@@ -1,6 +1,8 @@
 from tastypie.resources import ModelResource
 from tastypie.authorization import Authorization
+from tastypie import fields
 from models import Game,Move
+
 
 class GameResource(ModelResource):
     class Meta:
@@ -16,7 +18,22 @@ class CurrentGameResource(ModelResource):
         limit = 1
 
 class MoveResource(ModelResource):
+    game_id = fields.IntegerField()
+
     class Meta:
-        queryset = Move.objects.all()
+        queryset = Move.objects.all().order_by('-time')
         resource_name = "move"
         authorization = Authorization()
+
+    def dehydrate_game_id(self, bundle):
+        return bundle.obj.game.pk
+
+
+    def hydrate(self, bundle):
+        bundle.data['player']= int(bundle.data['player'])
+        bundle.data['value'] = int(bundle.data['value'])
+        return bundle
+
+    def obj_create(self, bundle, **kwargs):
+        return super(ModelResource,self).obj_create(bundle,game= Game.objects.get(pk=bundle.data.pop(
+                'game_id')))
